95c7ac18b58cceac08ca61696b69484da009ac80
JuanyiPG <jspinto57@soy.sena.edu.co>
2025-08-19 11:38:28 -0500

    Delete Pagina-Proyecto directory

--- Pagina-Proyecto/Pagina Web/Formularios/header-formularios.php
+++
@@ -1,8 +0,0 @@
-<div class="nav">
-    <ul>
-      <li class="m-left" id="list"><i class="bi bi-house-door-fill" onclick="window.history.back()"></i>
-      <a href="/Admin/index-admin.html"><img src="/IMG/logo rosa.jpeg" alt="Logo"></a>
-      </li>
-    </ul>
-  </div>
-  
