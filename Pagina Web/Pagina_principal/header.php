ee0e1b8e800e65d61e0c11d28335c0b45717377a
JuanyiPG <jspinto57@soy.sena.edu.co>
2025-09-01 16:08:05 -0500

    Update header.php

--- Pagina Web/Pagina_principal/header.php
+++ Pagina Web/Pagina_principal/header.php
@@ -6,7 +6,7 @@
           <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
         </svg>
       </div>
-      <a href="../../index.php"><img src="Pagina Web/IMG/logo rosa.jpeg" alt="Logo" class="logo-menu"></a>
+      <a href="../../index.html"><img src="Pagina Web/IMG/logo rosa.jpeg" alt="Logo" class="logo-menu"></a>
     </li>
     <li class="m-right">
     <a href="../Formularios/form.php" class="icon-link">
