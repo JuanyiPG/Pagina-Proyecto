147ac4704c1264a86b4b3f882023d051a16a6bd5
JuanyiPG <jspinto57@soy.sena.edu.co>
2025-09-17 00:33:39 -0500

    update documents

--- Pagina Web/Formularios/registro.php
+++ Pagina Web/Formularios/registro.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+  
   <title>Registro</title>
   <style>
     * {
@@ -13,6 +14,11 @@
     }
 
     body {
+      background-color: whitesmoke;
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+
+     body {
       background-color: whitesmoke;
       font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     }
@@ -29,41 +35,32 @@
     }
 
     .nav img{
-      height: 60px;
-      width: 90px;
-      margin-top: 5px ;
-      padding-top: 5px;
+      height: 90%;
+      width: 120px;
+      margin-top: 0px ;
+      padding-top: 0px;
     }
 
-    .nav ul li a i{
+    .nav ul {
       border-radius: 5px;
       padding: 1px 20px;
       width: 1000px;
-      height: 100px;
+      height: 80px;
       color: #000;
       font-size: 40px;
-    }
-    .nav ul li a i:hover{
-      background-color: #834343;
-      color: antiquewhite;
-      border-radius: 5px;
-      padding: 1px 20px;
-      width: 50px;
-      height: 100%;
     }
 
     .nav ul{
       display: flex;
     }
 
-
     form {
-      background-color: white;
+      background-color: whitesmoke;
       padding: 40px;
       border-radius: 20px;
-      width: 400px;
-      box-shadow: 0 4px 10px rgba(0,0,0,0.1);
-      margin: 150px auto 40px auto;
+      width: 650px;
+      box-shadow: 0 4px 10px rgba(82, 60, 60, 1);
+      margin: 100px auto 40px auto;
     }
 
     form h1 {
@@ -85,17 +82,12 @@
       width: 100%;
       outline: none;
     }
+
     form input:focus {
       border-color: #834343;
     }
-    form a{
-      position: flex;
-      background-color: transparent;
-      color:#8d103a;
 
-    }
-
-     form button {
+    form button {
       background-color:#5c0b26;
       border-radius: 20px;
       color:  white;
@@ -120,30 +112,62 @@
       margin-left: -3px;
     }
 
+    .camps{
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .colum-left, .colum-right {
+      display: inline-block;
+      width: 45%;
+      vertical-align: top;
+      margin: 0 5px;
+    }
+
   </style>
 </head>
 <body>
 
-<div class="nav">
+  <div class="nav">
     <ul>
-      <img src="../IMG/Logo_Luxy_Fashion.png" alt="Logo">
-      </li>
+      <a href="../../index.html">
+        <img src="../IMG/Logo_Luxy_Fashion.png" alt="Logo">
+      </a>
     </ul>
   </div>
 
-  <form action="../Admin/index-admin.html">
-    <h1>Iniciar Sesión</h1>
+  <form action="../Admin/index-admin.php">
+    <h1>Registro</h1>
     <hr>
-    <label for="usuario">Usuario</label>
-    <input type="text" required>
+    <div clas="camps">
 
-    <label for="password">Contraseña</label>
-    <input type="text" required>
-    <br><br>
-     <a href="Form_registro.php">¿No tiene una cuenta?</a>
-    <br><br>
-    <a href="#">¿Olvidó su Contraseña?</a>
-    <button>Ingresar</button>
+      <div class="colum-left">
+        <label for="name">Nombre</label>
+        <input type="text" pattern="[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+" title="Solo se permiten letras A-Z, a-z" required>
+        
+        <label for="second_name">Apellido</label>
+        <input type="text" pattern="[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+" title="Solo se permiten letras A-Z, a-z" required>
+        
+        <label for="correo">Correo</label>
+        <input type="email" name="correo" required>
+      </div>
+      <div class="colum-right">
+        <label for="date">Fecha de nacimiento</label>
+        <input type="date" name="datemind" id="datemind" min="2007-01-01" required>
+        
+        <label for="username" >Nombre de usuario</label>
+        <input type="text" required>
+        
+        <label for="password">Contraseña</label>
+        <input type="password" required>
+      </div>
+    </div>
+      <br><br>
+      <a href="Inicio-sesion.html">¿Ya tiene una cuenta?</a>
+      <br><br>
+    <button><p>Registrarse</p></button>
+
   </form>
+
 </body>
 </html>
