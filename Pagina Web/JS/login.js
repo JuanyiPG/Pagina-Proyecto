e8ff01883103bf7f7a0459b1988736bfb2ba1819
JuanyiPG <jspinto57@soy.sena.edu.co>
2025-09-18 01:55:43 -0500

    update documents login, index

--- Pagina Web/JS/login.js
+++ Pagina Web/JS/login.js
@@ -9,3 +9,28 @@ btnSignIn.addEventListener('click',()=>{
 btnSignUp.addEventListener('click',()=>{
     container.classList.add('toggle');
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const formSignIn = document.querySelector(".sign-in");
+
+  formSignIn.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+
+    const email = formSignIn.querySelector("input[placeholder='Email']").value.trim();
+    const password = formSignIn.querySelector("input[placeholder='Contraseña']").value.trim();
+
+    const users = [
+      { email: "juanpintow@gmail.com", password: "12345" },
+      { email: "admin@correo.com", password: "admin" }
+    ];
+
+    const usuarioValido = users.find(u => u.email === email && u.password === password);
+
+    if (usuarioValido) {
+      window.location.href = "../Admin/index-admin.html"; 
+    } else {
+      alert("Usuario o contraseña incorrectos ");
+    }
+  });
+});
+
