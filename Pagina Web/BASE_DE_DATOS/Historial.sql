b2593c2494d4b630fe596c07cbe33b9208367429
yulieth07 <214584437+yulieth07@users.noreply.github.com>
2025-09-22 19:15:31 -0500

    add

--- Pagina Web/BASE_DE_DATOS/Historial.sql
+++ Pagina Web/BASE_DE_DATOS/Historial.sql
@@ -1,5 +1,42 @@
 CREATE TABLE historial_empleados (
-id_historial int primary key auto_increment,
-accion varchar(2000) not null,
- fecha_modificacion datetime not null
+id_historial_emple int primary key auto_increment,
+id_empleado int not null,
+direccion varchar(100),
+telefono int,
+correo varchar(100),
+salario float, 
+estado varchar(100),
+fecha_modificacion datetime not null
 ); 
+
+CREATE TABLE historial_cliente (
+id_historial_cli int primary key auto_increment,
+id_cliente int not null, 
+direccion_cli varchar(100),
+telefono_cli int,
+correo_cli varchar(100)
+);
+
+CREATE TABLE historial_matpri (
+id_historial_matpri int primary key auto_increment,
+id_matpri int not null, 
+stock_actual_matpri varchar(100),
+stock_min_matpri decimal(10,0),
+ estado_matpri decimal(10,0)
+);
+
+CREATE TABLE historial_pedido (
+id_historial_pedido int primary key auto_increment,
+id_pedido int not null, 
+color_p varchar(50),
+cant_p decimal(10,0), 
+estado_p varchar(50)
+);
+
+CREATE TABLE historial_produccion (
+id_historial_produccion int primary key auto_increment,
+id_produccion int not null, 
+costo_manobra bigint, 
+valor_iva bigint 
+);
+
--- Pagina Web/BASE_DE_DATOS/triggers.sql
+++ Pagina Web/BASE_DE_DATOS/triggers.sql
@@ -1,30 +1,71 @@
  DELIMITER //
- create trigger actualizar_empleado after update on empleado 
- for each row begin 
- 
- if old.direccion_empleado <> new.direccion_empleado then 
-insert into historial_empleados (id_empleado, campo_modificado, valor_anterior, valor_nuevo, fecha_modificacion )
-values (old.id_empleado, 'direccion_empleado', old.direccion_empleado, new.direccion_empleado, now());
-end if; 
+ create trigger act_empleado 
+ after update on empleado 
+ for each row 
+ begin 
+ if old.direccion_empleado <> new.direccion_empleado or  
+    old.telefono_empleado <> new.telefono_empleado or  
+    old.correo_empleado <> new.correo_empleado or   
+    old.salario_empleado <> new.salario_empleado  or 
+    old.Estado_empleado <> new.Estado_empleado then 
+ insert into  historial_empleado (id_empleado, direccion_empl, telefono_emple, correo_emple, salario_emple, estado_emple)
+ values (old.id_empleado, new.direccion_empleado, new.telefonoa_empleado, new.correo_empleado, new.salario_empleado, new.Estado_empleado);
+end if ;
+end;    
+ DELIMITER; 
 
-if old.telefono_empleado <> new.telefono_empleado then 
-insert into historial_empleado (id_empleado, campo_modificado, valor_anterior, valor_nuevo, fecha_modificacion)
-values (old.id_empleado, 'telefono_empleado', old.telefono_empleado, new.telefono_empleado, now()); 
-end if; 
-
-if old.correo_empleado <> new.correo_empleado then 
-insert into historial_empleado (id_empleado, campo_modificado, valor_anterior, valor_nuevo, fecha_modificaion)
-values (old.id_empleado, 'correo_empleado', old.correo_empleado, new.correo_empleado, now());
-end if;
-
-if old.Fecha_ing_empleado <> new.Fecha_ing_empleado then 
-insert into historial_empleado (id_empleado, campo_modificado, valor_anterior, valor_nuevo, fecha_modificaion)
-values (old.id_empleado, 'Fecha_ing_empleado', old.Fecha_ing_empleado, new.Fecha_ing_empleado, now());
-end if;
-
-if old.salario_empleado <> new.salario_empleado then 
-insert into historial_empleado (id_empleado, campo_modificado, valor_anterior, valor_nuevo, fecha_modificaion)
-values (old.id_empleado, 'salario_empleado', old.salario_empleado, new.salario_empleado, now());
+ DELIMITER //
+create trigger act_cliente
+after update on cliente
+for each row 
+begin 
+if old.direccion_cliente <> new.direccion_cliente or 
+   old.telefono_cliente <> new.telefono_cliente or 
+   old.correo_cliente <> new.telefono_cliente then
+insert into historial_cliente(id_cliente, direccion_cli, telefono_cli, correo_cli)
+values (old.id_cliente, new.direccion_cliente, new.telefono_cliente, new.correo_cliente);
 end if;
 end; 
+ DELIMITER; 
+ 
+  DELIMITER //
+create trigger act_materia_prima
+after update on materia_prima
+for each row 
+begin 
+if old.stock_actual_materia_p <> new.stock_actual_materia_p or 
+old.stock_minimo_materia_p <> new.stock_minimo_materia_p or 
+old.estado_materia_p <> new.estado_materia_p then
+insert into historial_matpri(id_matpri, stock_actual_matpri, stock_min_matpri, estado_matpri )
+values (old.materia_p, new.stock_actual_materia_p, new.stock_minimo_materia_p, new.estado_materia_p);
+end if;
+end; 
+ DELIMITER;  
+
+ DELIMITER //
+create trigger act_pedido
+after update on pedido 
+for each row 
+begin 
+if old.color_p_pedido <> new.color_p_pedido or 
+old.cant_producto <> new.cant_producto or 
+old.estado_pedido <> new.estado_pedido then
+insert into historial_pedido(id_pedido, color_p, cant_p, estado_p)
+values (old.id_pedido, new.color_p_pedido, new.cant_producto, new.estado_pedido);
+end if;
+end; 
+ DELIMITER;  
+
+create triggers act_produccion
+after update on produccion 
+for each row 
+begin 
+if old.costo_mano_obra <> new.costo_mano_obra or 
+old.valor_iva <> new.valor_iva then
+insert into hostorial_produccion(id_produccion, costo_manobra, valor_iva)
+values (old.id_produccion, new.costo_mano_obra, new.valor_iva); 
+end if; 
+end; 
+ DELIMITER; 
+
  
