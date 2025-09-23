0490ee3f96b3e07cb55c18c80840ef733ce6cc1b
JuanyiPG <jspinto57@soy.sena.edu.co>
2025-09-22 19:26:32 -0500

    update documents

--- Pagina Web/Items/header-cliente.php
+++ Pagina Web/Items/header-cliente.php
@@ -9,11 +9,11 @@
         <!--Buscador------->
         <div class="search">
             <label for="b-Search">
+                <div class="Buscador">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
                 </svg>
             </label>
-            <div class="Buscador">
                 <input type="search" placeholder="Buscar.." id="b-Search" name="b-Search">
             </div>
         </div>
