7dc82155f5078ea447332804acc1d5d741400a38
Lauracmg <laurac_murillog@soy.sena.edu.co>
2025-09-23 22:01:01 -0500

    create
    Agregar la carpeta de el formulario de factura venta

---
+++ Pagina Web/PHP/FACTURA_COMPRA/INDEX_FACTURAC.php
@@ -0,0 +1,94 @@
+<?php 
+require_once "CONEXION.php"; 
+require_once "CLASE_FACTURAC.PHP";  
+
+$datos = [];
+$obj = new FACTURA_C();
+    
+if (isset($_GET['search'])) {
+    $search = $_GET['search'];
+    $datos = $obj->ConsultarfacturactPorID($search); 
+} else {
+    $datos = $obj->CONSULTAR_FACTURA_C(); 
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Fctura compra</title>
+  <link rel="stylesheet" href="../CSS/CSS.css">
+  </head>
+<body>
+
+  <div class="menulateral">
+    <div class="logo">
+      <img src="../proyecto/20250906_163741.png" alt="Logo Luxy">
+    </div>
+    <hr>
+    <ul>
+      <li class="active">Inicio</li>
+      <li>Formulario1</li>
+      <li>Formulario2</li>
+      <li>Formulario3</li>
+      <li>Formulario4</li>
+    </ul>
+  </div>
+
+  <div class="main">
+    <div class="card">
+      <h1>Gestión de factura compra</h1>
+
+     
+      <form action="INSERTAR_FACRURAC.PHP" method="post">
+        <div class="form-group">
+          <input type="text" name="cod_factura_compra" placeholder="Código" required>
+          <input type="date" name="fecha_factura_compra" placeholder="Fecha" required>
+        </div>
+        <div class="form-group">
+          <input type="text" name="total_faactura_compra" placeholder="total de la factura"required>
+          <input type="text" name="metododepago_factura_compra" placeholder="Metodo de pago"required>
+        </div>
+        <div class="form-group">
+          <input type="text" name="estado_factura_compra" placeholder="Estado"required>
+          <input type="text" name="id_empleado_fk_factura_compra" placeholder="Identificador de el empleado que regustro la factura compra"required>
+        </div>
+        <button type="submit" class="save-btn">Insertar</button>
+      </form>
+    </div>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Código</th>
+          <th>Fecha</th>
+          <th>Total</th>
+          <th>Metodo de pago</th>
+          <th>estado</th>
+          <th>Identificador</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($datos as $row) { ?>
+          <tr>
+            <td><?php echo $row['cod_factura_compra']; ?></td>
+            <td><?php echo $row['fecha_factura_compra']; ?></td>
+            <td><?php echo $row['total_faactura_compra']; ?></td>
+            <td><?php echo $row['metododepago_factura_compra']; ?></td>
+            <td><?php echo $row['estado_factura_compra']; ?></td>
+            <td><?php echo $row['id_empleado_fk_factura_compra']; ?></td>
+            <td>
+              <a class="btn-accion" href="EDITAR_FACTURAC.php?cod_factura_compra=<?php echo $row['cod_factura_compra']; ?>">Actualizar</a>
+              <a class="btn-accion" href="ELIMINAR_FACTURAC.php?cod_factura_compra=<?php echo $row['cod_factura_compra']; ?>">Eliminar</a>
+            </td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
+
+</body>
+</html>
