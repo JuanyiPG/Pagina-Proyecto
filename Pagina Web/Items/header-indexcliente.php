5ab76dae3cfef6d379f2d8fe1feeab60c80f5669
JuanyiPG <jspinto57@soy.sena.edu.co>
2025-09-29 02:09:50 -0500

    update documents

--- Pagina Web/Items/header-indexcliente.php
+++ Pagina Web/Items/header-indexcliente.php
@@ -1,51 +1,35 @@
-<header class="navbar-cliente" id="header-cliente">
-  <nav>
-    <div class="menu">
-      <!-- Izquierda -->
-      <div class="menu-l">
-        <button id="btnHamburguesa" class="btn btn-link p-0 border-0">
-          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" 
-            class="bi bi-list" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" 
-              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 
-              0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 
-              0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-          </svg>
-        </button>
 
+<div class="navbar-cliente">
+  <nav id="mainNav" class="main-nav">
+    <div class="menu">
+      <div class="menu-l">
+        <button id="btnHamburguesa" class="btn-hamburger" aria-label="Menú">
+          <!-- icono -->
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" 
+               class="bi bi-list" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/></svg>
+        </button>
         <a href="index-cliente.php">
-          <img src="../IMG/Logo_Luxy_Fashion.png" alt="Logo">
+          <img src="../IMG/Logo_Luxy_Fashion.png" alt="Logo" class="logo">
         </a>
       </div>
 
-      <!-- Derecha -->
-      <div class="menu-r">
-        <!-- Carrito -->
-        <a href="#">
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" 
-            class="bi bi-bag-heart" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" 
-              d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5zm1 
-              0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 
-              3.5 0 1 1 7 0M14 14V5H2v9a1 1 0 0 0 1 
-              1h10a1 1 0 0 0 1-1M8 7.993c1.664-1.711 
-              5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 
-              0-5.132"/>
-          </svg>
-        </a>
-
-        <!-- Login -->
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" 
-            class="bi bi-person-circle" viewBox="0 0 16 16">
+        <div class="menu-usuario">
+          <svg id="btnUsuario" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-            <path fill-rule="evenodd" 
-              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 
-              8m8-7a7 7 0 0 0-5.468 11.37C3.242 
-              11.226 4.805 10 8 10s4.757 1.225 
-              5.468 2.37A7 7 0 0 0 8 1"/>
+            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
           </svg>
+
+          <div id="menuUsuario" class="menu-usuario-dropdown hidden">
+            <button id="btnCerrarSesion">Cerrar sesión</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <div id="hamburgerMenu" class="hamburger-menu hidden" aria-hidden="true">
+      <div class="hamburger-inner">
       </div>
     </div>
   </nav>
-
-</header>
+</div>
