542b42fb905f43e76b74d5ee3073349e98f6c932
JuanyiPG <jspinto57@soy.sena.edu.co>
2025-09-28 22:56:10 -0500

    update index-admin.php

--- Pagina Web/PHP/index-admin.php
+++ Pagina Web/PHP/index-admin.php
@@ -10,12 +10,15 @@ require_once "../Items/header-adminn.php"
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   <script rel="stylesheet" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
   <title>Admin Dashboard</title>
   <link rel="stylesheet" href="../CSS/index.css">
     <body class="body-3">
   <div class="main-1">
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -266,6 +269,9 @@ require_once "../Items/header-adminn.php"
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
@@ -277,6 +283,7 @@ require_once "../Items/header-adminn.php"
       <h1>Bienvenido al panel de administración</h1>
     </div>
 
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
@@ -295,6 +302,8 @@ require_once "../Items/header-adminn.php"
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
     <div class="cards">
       <div class="card">
         <h3>Usuarios</h3>
@@ -307,6 +316,9 @@ require_once "../Items/header-adminn.php"
       <div class="card">
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
@@ -356,6 +368,7 @@ require_once "../Items/header-adminn.php"
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   // Llamar al cargar la página
   actualizarNotificaciones();
 
@@ -380,6 +393,8 @@ require_once "../Items/header-adminn.php"
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
   <!-- Estadísticas dinámicas -->
   <script>
     document.addEventListener("DOMContentLoaded", function() {
@@ -398,6 +413,9 @@ require_once "../Items/header-adminn.php"
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
