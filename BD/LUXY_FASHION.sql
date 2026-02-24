1886dc521af68dd7b584fa0f6b9f686882bcf3b9
JuanyiPG <jspinto57@soy.sena.edu.co>
2026-02-23 20:09:33 -0500

    Update LUXY_FASHION.sql

--- BD/LUXY_FASHION.sql
+++ BD/LUXY_FASHION.sql
@@ -38,19 +38,19 @@ CREATE TABLE Cliente (
   dir_clien VARCHAR(100) NOT NULL,
   tel_clien VARCHAR(20) NOT NULL,
   correo_clien VARCHAR(100) NOT NULL UNIQUE,
-  id_rol_fk_clien INT NOT NULL,
-  CONSTRAINT fk_clien_rol  FOREIGN KEY (id_rol_fk_clien) REFERENCES Rol(id_rol)
 );
 
 -- =========================
 -- TABLA USUARIOS
 -- =========================
-CREATE TABLE usuarios (
+CREATE TABLE usuario (
   id_usuario INT PRIMARY KEY AUTO_INCREMENT,
   username VARCHAR(50) NOT NULL UNIQUE,
   contrasena VARCHAR(255) NOT NULL,
   id_emple_fk_usuario INT NULL,
   id_clien_fk_usuario INT NULL,
+  id_rol_fk_usuario int NOT NULL,
+  CONSTRAINT fk_usuario_rol FOREIGN KEY (id_rol_fk_usuario) REFERENCES Rol(id_rol),
   CONSTRAINT fk_usuario_emple FOREIGN KEY (id_emple_fk_usuario) REFERENCES Empleado(id_emple) ON DELETE CASCADE,
   CONSTRAINT fk_usuario_clien FOREIGN KEY (id_clien_fk_usuario) REFERENCES Cliente(id_clien) ON DELETE CASCADE
 );
