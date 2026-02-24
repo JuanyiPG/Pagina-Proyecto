0aefe11bbe2e0cfbed189ea11faba02d4e9310b4
JuanyiPG <jspinto57@soy.sena.edu.co>
2026-02-23 20:18:59 -0500

    Update LUXY_FASHION.sql

--- BD/LUXY_FASHION.sql
+++ BD/LUXY_FASHION.sql
@@ -232,5 +232,5 @@ INSERT INTO rol (nom_rol)
 VALUES ('ADMIN');
 
 
-INSERT INTO usuario (username, contrasena, id_rol)
+INSERT INTO usuario (username, contrasena, id_rol_fk_usuario)
 VALUES ('admin', '$2a$10$XyZp9F8sLk2JmNoPqRstUeH1JkLmNoPqRsTuVwXyZ123456789',1);
