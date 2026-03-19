451a99de163b1e8c7923b72b13bc47ba9482295c
yulieth07 <yulieth_junnco@soy.sena.edu.co>
2026-03-18 20:50:01 -0500

    update and add

--- Pagina Python/LuxyFashion/urls.py
+++ Pagina Python/LuxyFashion/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+
+from LuxyFashion import settings
 
 from . import views
 
@@ -26,3 +29,5 @@ urlpatterns = [
     path('usuarios/', include('usuarios.urls'))
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
