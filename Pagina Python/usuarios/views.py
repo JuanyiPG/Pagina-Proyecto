4bb1a053370bb9f9ab28118668b741e03ca06b93
Laura-CMG <yujishin42@gmail.com>
2026-03-27 00:39:52 -0500

    update

--- Pagina Python/usuarios/views.py
+++ Pagina Python/usuarios/views.py
@@ -165,20 +165,14 @@ def eliminar_empleado(request, id):
     return redirect('lista_empleados')
 
 
-<<<<<<< HEAD
-=======
 @never_cache
 @login_required
->>>>>>> 16d15749ef8ea742fe75a022afde8c975a926237
 def lista_clientes(request):
     clientes = Cliente.objects.all()
     return render(request, 'usuarios/clientes/lista.html', {'clientes': clientes})
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 16d15749ef8ea742fe75a022afde8c975a926237
 def crear_cliente(request):
     usuarios = Usuario.objects.all()
 
@@ -221,10 +215,7 @@ def editar_cliente(request, id):
     })
 
 
-<<<<<<< HEAD
-=======
 #  ELIMINAR
->>>>>>> 16d15749ef8ea742fe75a022afde8c975a926237
 def eliminar_cliente(request, id):
     cliente = get_object_or_404(Cliente, id_clien=id)
     cliente.delete()
