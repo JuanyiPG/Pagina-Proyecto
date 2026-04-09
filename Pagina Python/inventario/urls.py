c76aa8b8983eddd138e686a4214994f9a73a46ab
JuanyiPG <JuanPintoW@gmail.com>
2026-04-09 11:57:57 -0500

    Update urls.py

--- Pagina Python/inventario/urls.py
+++ Pagina Python/inventario/urls.py
@@ -9,10 +9,12 @@ urlpatterns=[
     path('proveedores/editar/<int:id>/', views.editar_provee, name="editar_provee"),
     path('proveedores/eliminar/<int:id>/', views.eliminar_provee, name="eliminar_provee"),
 
-    #Materia Prima
+    #Movimiento Materia Prima
     path('mmtp/', views.lista_mmtp, name="lista_mmtp"),
     path('mmtp/editar/<int:id>/', views.editar_mmtp, name="editar_mmtp"),
     path('mmtp/eliminar/<int:id>/', views.eliminar_mmtp, name="eliminar_mmtp"),
+    path('mmtp/api/import_archive/', views.carga_masiva, name='import_excel'),
+    path('mmtp/PDF/reports', views.report_mmtp, name="descarga_reporte_mmtp"),
 
     # Estampados
     path('estampados/', views.lista_estampado, name="lista_estampado"),
