fec699b3c95c7f19820c70d5e789a135a116c799
PabloDavidVega <JuanPintoW@gmail.com>
2026-05-20 10:43:38 -0500

    Revert "Update tests.py"
    This reverts commit bd1c9523e015429d7b5644bd89e072eabc07a7a3.

--- Pagina Python/inventario/tests.py
+++ Pagina Python/inventario/tests.py
@@ -1,33 +1,3 @@
 from django.test import TestCase
-from . import reverse
-from . models import Proveedor
+
 # Create your tests here.
-class  ProveedorViewsTest(TestCase):
-    def setUp(self):
-        #Crear persona de prueba
-        self.proveedor=Proveedor.objects.create(
-            documento=80160,
-            nombre='Carlos',
-            apellido='Rodriguez',
-            direccion='Calle 65',
-            email='rodri@gmail.com'
-            )
-        
-    def test_incio(self):
-        response=self.client.get(reverse('inicio'))
-        
-        self.assertEqual(response.status_code,200)
-        self.assertIn('opciones_edad',response.context)
-        self.assertTemplateUsed(response, 'persona/registro.html')
-        
-    def text_registro(self):
-        response=self.client.post(reverse('registrar_persona'), {
-                'documento':80160,
-                'nombre':'Carlos',
-                'apellido':'Rodriguez',
-                'direccion':'Calle 65',
-                'email':'rodri@gmail.com'})
-        
-        self.assertEqual(Proveedor.objects.count(),2)
-        self.assertEqual(Proveedor.objects.get(documento=80160).nombre, 'Carlos')
-        self.assertEqual(response.status_code, 302)
